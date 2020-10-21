3a9f24ac414acd7c613dc1f8b2eebaae2b6b4316
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-21 16:04:48 -0400

    adds fetch request to starwars api

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+
+  constructor(){
+    super()
+    this.state = {
+      loading: false, 
+      character: {}
+    }
+  }
+
+  componentDidMount(){
+    this.setState({loading: true })
+    fetch("https://swapi.dev/api/people/1")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          loading: false,
+          character: data
+        })
+      })
+  }
+
+
+  render(){
+    const text = this.state.loading ? "loading" : this.state.character.name
+    return (
+      <div className="App">
+        {text}
+      </div>
+    );
+  }
+}
+
+export default App;
