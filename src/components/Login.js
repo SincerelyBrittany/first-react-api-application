1ea9a955abfb5c3e5c6da9d81587236326c1fe70
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-21 16:34:47 -0400

    adds a checkbox and on change terinary to check when a button is checked or not

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,12 +5,16 @@ export default class Login extends Component {
         super()
         this.state ={
             firstName: "",
-            lastName: ""
+            lastName: "",
+            isFriendly: true
         }
     }
 
     handleChange = (event) => {
-        const {name, value} = event.target
+        const {name, value, type, checked} = event.target
+        type === "checkbox" ? 
+        this.setState({ [name]: checked})
+        :
         this.setState({
             [name]: value
         })
@@ -23,9 +27,31 @@ export default class Login extends Component {
         return(
             <div className="form-div">
             <form>
-                <input type="text" name="firstName" value={this.state.firstName} placeholder="First Name" onChange={this.handleChange}/>
-                <input type="text" name="lastName" value={this.state.lastName} placeholder="Last Name" onChange={this.handleChange}/>
+                <input 
+                    type="text" 
+                    name="firstName" 
+                    value={this.state.firstName} 
+                    placeholder="First Name" 
+                    onChange={this.handleChange}
+                />
+                <input 
+                    type="text" 
+                    name="lastName" 
+                    value={this.state.lastName} 
+                    placeholder="Last Name" 
+                    onChange={this.handleChange}
+                />
                 <h1>{this.state.firstName} {this.state.lastName}</h1>
+
+                <textarea value={"Some default value"}/>
+
+                
+                <input 
+                type="checkbox"
+                name="isFriendly"
+                checked={this.state.isFriendly}
+                onChange={this.handleChange}
+                /> Is friendly?
             </form>
             </div>
         )
